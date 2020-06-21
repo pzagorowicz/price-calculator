63a7f06791915574d3d39de07a6a6a5841efc9ef
Piotr Zagórowicz <piotr.zagorowicz@gmail.com>
2020-06-21 19:28:54 +0200

    deselectig services

--- src/index.ts
+++ src/index.ts
@@ -8,27 +8,62 @@ export const updateSelectedServices = (
     
     switch (action.type) {
         case "Select":
-            if (previouslySelectedServices.some(value => value === action.service)) {
-                break;
+            {
+                if (previouslySelectedServices.some(value => value === action.service)) {
+                    break;
+                }
+
+                if (action.service === "BlurayPackage" && !previouslySelectedServices.some(value => value === "VideoRecording")) {
+                    break;
+                }
+
+                if (action.service === "TwoDayEvent" && !previouslySelectedServices.some(value => value === "Photography" || value === "VideoRecording")) {
+                    break;
+                }
+
+                const services = [...previouslySelectedServices];            
+                services.push(action.service);
+                return services;
             }
 
-            if (action.service === "BlurayPackage" && !previouslySelectedServices.some(value => value === "VideoRecording")) {
-                break;
-            }
+        case "Deselect":
+            {
+                const index = previouslySelectedServices.indexOf(action.service);
 
-            if (action.service === "TwoDayEvent" && !previouslySelectedServices.some(value => value === "Photography" || value === "VideoRecording")) {
-                break;
-            }
+                if (index > -1) {
+                    let services = [...previouslySelectedServices];
+                    services.splice(index, 1);
 
-            const services = [...previouslySelectedServices];            
-            services.push(action.service);
-            return services;
-    
-        default:
-            break;
+                    if (action.service === "VideoRecording") {
+                        removeFromArray(services, "BlurayPackage");
+                    }
+                    
+                    if (action.service === "Photography" || action.service === "VideoRecording") {
+                        const anotherMainServiceExists = services.some(service => service === "Photography" || service === "VideoRecording");
+                        if (!anotherMainServiceExists) {
+                            removeFromArray(services, "TwoDayEvent");
+                        }
+                    }
+
+                    return services;
+                }
+                
+                break;                
+            }
     }
 
     return previouslySelectedServices;
 };
 
 export const calculatePrice = (selectedServices: ServiceType[], selectedYear: ServiceYear) => ({ basePrice: 0, finalPrice: 0 });
+
+const removeFromArray = (services: ServiceType[], service: ServiceType) => {
+    const index = services.indexOf(service);
+
+    if (index > -1) {
+        services.splice(index, 1);
+        return true;
+    }
+
+    return false;
+}
